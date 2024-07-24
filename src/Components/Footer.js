38100ccbb3ba14ac5c38c4b0a9e13b667e3df2f4
Aaron S <aaronsimon2612@gmail.com>
2024-07-24 22:33:55 +0530

    Updated the page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import { FaFacebook, FaHome, FaInstagram, FaLinkedin, FaMailBulk, FaPhone } from
 import "./Footer.css";
 
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -28,7 +29,7 @@ const Footer = () => {
                 <h4>About the company</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </p>
                 <div className="social">
-                <FaFacebook size={30} style={{color:"white", marginRight:"1rem"}}/>
+                <Link to="https://www.facebook.com/share/vzsfe59KN4JF8Ryv/?mibextid=qi2Omg"><FaFacebook size={30} style={{color:"white", marginRight:"1rem"}}/></Link>
                 <FaInstagram size={30} style={{color:"white", marginRight:"1rem"}}/>
                 <FaLinkedin size={30} style={{color:"white", marginRight:"1rem"}}/>
                 </div>
